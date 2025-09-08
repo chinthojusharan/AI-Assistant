8af7684d6e50376511982e15b48e1b617c32b93f
Sharan Chinthoju <158158398+chinthojusharan@users.noreply.github.com>
2025-09-08 20:50:53 +0530

    Updated index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js"
 const app = express()
 const port = process.env.PORT || 5000
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://ai-assistant-maqk.onrender.com",
     credentials:true
 }))
 app.use(express.json())
